489b5d2799da3621fffbabc92d326f91db737f95
BullThistle <rfurry@gmail.com>
2022-08-12 01:32:52 -0500

    enable purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   purge: {
+    enabled: true,
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ],
     options: {
       safelist: ['dark']
     }
