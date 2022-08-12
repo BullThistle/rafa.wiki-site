82258b444155c52d9c0240825f3ed1246cd9bd46
BullThistle <rfurry@gmail.com>
2022-08-11 22:14:34 -0500

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export const Footer = () => (
-  <div className='text-center border-t border-slate-300 p-4 text-slate-400'>
-    <div>This is the footer</div>
+  <div className='text-center border-t border-slate-300 p-4 text-slate-400 flex justify-between text-sm'>
+    <div>© 2022 by Rafael Furry</div>
+    <div className='text-sm hover:cursor-pointer'>
+      <Link href='https://github.com/BullThistle/portfolio'>source code</Link>
+    </div>
   </div>
 )
