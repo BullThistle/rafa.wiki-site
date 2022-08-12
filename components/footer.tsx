e5f2e6726dad0ae30373be3ac3ac5e82f0c93e96
BullThistle <rfurry@gmail.com>
2022-08-12 08:15:34 -0500

    styling updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Footer = () => (
   <div className='border-t-2 border-light-gray dark:border-gray pt-4 px-2 flex justify-between text-sm'>
-    <div>© 2022 by Rafael Furry</div>
+    <div className='text-gray'>© 2022 by Rafael Furry</div>
     <div className='text-sm hover:cursor-pointer text-purple'>
       <Link href='https://github.com/BullThistle/portfolio'>source code</Link>
     </div>
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,27 +2,23 @@ import { ThemeToggle } from './theme-toggle'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const selectedStyles = 'text-purple'
-const unselectedStyles = 'hover:purple dark:text-white'
-
 export const Navbar = () => {
   const { pathname } = useRouter()
   return (
     <nav className='py-3'>
       <div className='container py-3 flex justify-between mx-auto max-w-xs sm:max-w-md md:max-w-lg'>
-        <div className='mb-1 self-center text-xl font-semibold'>
+        <div className='mb-1 self-center text-xl font-semibold text-purple'>
           <Link href='/'>rafa.wiki</Link>
         </div>
         <div className='w-full pl-3 text-gray-700'>
           <ul className='flex flex-row p-4 space-x-8 mt-0 text-sm font-medium'>
-            <li
-              className={pathname === '/' ? selectedStyles : unselectedStyles}>
+            <li className={`${pathname === '/' && 'underline'} text-purple`}>
               <Link href='/'>Home</Link>
             </li>
             <li
-              className={
-                pathname === '/resume' ? selectedStyles : unselectedStyles
-              }>
+              className={`${
+                pathname === '/resume' && 'underline'
+              } text-purple`}>
               <Link href='/resume'>Resume</Link>
             </li>
           </ul>
