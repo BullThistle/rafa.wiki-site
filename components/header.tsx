e609be28cb471947a4fcac1673e45119e2acdbfc
BullThistle <rfurry@gmail.com>
2022-08-11 22:23:20 -0500

    update borders

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => (
-  <div className='text-center border-b border-slate-300 p-4'>
+  <div className='text-center border-b-2 border-light-gray dark:border-gray p-4'>
     <div className='text-2xl font-bold'>Rafael Furry</div>
     <div className='text-gray-500'>Software Developer</div>
   </div>
