8e926d2576d55ab6b2cd8d38857ff49330a8c21c
BullThistle <rfurry@gmail.com>
2022-08-12 19:39:56 -0500

    add photo to home

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
 export const Header = () => (
-  <div className='text-center border-b-2 border-light-gray dark:border-gray p-4'>
-    <div className='text-2xl font-bold'>Rafael Furry</div>
-    <div className='text-gray'>Software Developer</div>
+  <div className='flex items-center justify-between border-b-2 border-light-gray dark:border-gray p-4'>
+    <div className='align-center'>
+      <div className='text-2xl font-bold'>Rafael Furry</div>
+      <div className='text-gray'>Software Developer</div>
+    </div>
+    <div className='w-4/12 px-4'>
+      <Image
+        src='/nw-beach.jpeg'
+        alt='relaxing at north west beach'
+        width='500'
+        height='500'
+        className='shadow rounded-full max-w-full h-auto align-middle border-none'
+      />
+    </div>
   </div>
 )
